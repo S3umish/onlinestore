02699c515c6b7a9f5922ab8d30be105c1bd24159
sunitee <suniteebajpai2@gmail.com>
2021-09-02 14:03:52 -0400

    initial files and folder setup

---
+++ src/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+// import styled from 'styled-components'
+
+
+export default function Cart() {
+    return (
+
+        <div>
+            Cart
+        </div>
+    )
+}
